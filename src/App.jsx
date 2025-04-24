ff7439b4446dc2bedd1af348c0546db50eda6fa0
neamoth <neamoth15-8202@diu.edu.bd>
2025-04-25 04:16:25 +0600

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import ProjectSection from './sections/ProjectSection'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <>
+      < NavBar />
       < Hero />
       < ProjectSection />
     </>
