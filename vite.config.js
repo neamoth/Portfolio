fc2544cf928cef8acdbf97f109279cb5707b16e3
neamoth <neamoth15-8202@diu.edu.bd>
2025-04-26 18:23:54 +0600

    remove base from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [
     tailwindcss(),
     react()
   ],
   build: {
-    chunkSizeWarningLimit: 500000, // sets the warning limit to 1000kb
+    chunkSizeWarningLimit: 5000, // sets the warning limit to 1000kb
   },
   server: {
     host: true,
